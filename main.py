9cd86745f7f33dd99908dc84f82c31841e36b5c0
Maxx <maximenorval@gmail.com>
2026-07-24 10:00:51 +0200

    description of the app

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+An application where the user gives a web URL to an LLM, which then acts as a
+salesperson in a conversation with the customer to sell the product, service,
+brand or person's skill featured on the web page. The conversation is between
+the LLM (as the salesperson) and the user (as the customer or prospect).
+"""
